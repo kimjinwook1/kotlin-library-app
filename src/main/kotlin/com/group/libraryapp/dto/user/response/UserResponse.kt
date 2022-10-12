c178a06d37583d110228cee213c54f2937607bbc
kimjinwook1 <rlawlsdnr628@naver.com>
2022-10-12 22:04:10 +0900

    dto Kotlin 전환

--- src/main/kotlin/com/group/libraryapp/dto/user/response/UserResponse.kt
+++ src/main/kotlin/com/group/libraryapp/dto/user/response/UserResponse.kt
@@ -1,30 +1,27 @@
-package com.group.libraryapp.dto.user.response;
+package com.group.libraryapp.dto.user.response
 
+import com.group.libraryapp.domain.user.User
 
-import com.group.libraryapp.domain.user.User;
+data class UserResponse(
+    val id: Long,
+    val name: String,
+    val age: Int?,
+) {
+    // 정적 팩토리 메서드 사용
+    companion object {
+        fun of(user: User): UserResponse {
+            return UserResponse(
+                id = user.id!!,
+                name = user.name,
+                age = user.age,
+            )
+        }
+    }
 
-public class UserResponse {
-
-  private final long id;
-  private final String name;
-  private final Integer age;
-
-  public UserResponse(User user) {
-    this.id = user.getId();
-    this.name = user.getName();
-    this.age = user.getAge();
-  }
-
-  public long getId() {
-    return id;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public Integer getAge() {
-    return age;
-  }
-
+//    부생성자를 사용하는 방법
+//    constructor(user: User): this(
+//        id = user.id!!, <- !! 들어간 이유는 처음에 null이 들어갈 수 있기 때문이다.
+//        name = user.name,
+//        age = user.age,
+//    )
 }
