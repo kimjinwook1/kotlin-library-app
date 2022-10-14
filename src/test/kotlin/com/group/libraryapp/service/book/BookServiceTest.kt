350a5f60dd9076ef15c3bed6e2898918ff271ce9
kimjinwook1 <rlawlsdnr628@naver.com>
2022-10-14 17:13:50 +0900

    Boolean에 Enum 적용

--- src/test/kotlin/com/group/libraryapp/service/book/BookServiceTest.kt
+++ src/test/kotlin/com/group/libraryapp/service/book/BookServiceTest.kt
@@ -7,6 +7,7 @@ import com.group.libraryapp.domain.user.User
 import com.group.libraryapp.domain.user.UserRepository
 import com.group.libraryapp.domain.user.loanhistory.UserLoanHistory
 import com.group.libraryapp.domain.user.loanhistory.UserLoanHistoryRepository
+import com.group.libraryapp.domain.user.loanhistory.UserLoanStatus
 import com.group.libraryapp.dto.book.request.BookLoanRequest
 import com.group.libraryapp.dto.book.request.BookRequest
 import com.group.libraryapp.dto.book.request.BookReturnRequest
@@ -64,7 +65,7 @@ class BookServiceTest @Autowired constructor(
         assertThat(results).hasSize(1)
         assertThat(results[0].bookName).isEqualTo("이상한 나라의 앨리스")
         assertThat(results[0].user.id).isEqualTo(savedUser.id)
-        assertThat(results[0].isReturn).isFalse
+        assertThat(results[0].status).isEqualTo(UserLoanStatus.LOANED)
     }
 
     @Test
@@ -73,7 +74,7 @@ class BookServiceTest @Autowired constructor(
         // given
         val saveBook = bookRepository.save(Book.fixture("이상한 나라의 앨리스"))
         val savedUser = userRepository.save(User("김진욱", null))
-        userLoanHistoryRepository.save(UserLoanHistory(savedUser, saveBook.name, false))
+        userLoanHistoryRepository.save(UserLoanHistory.fixture(savedUser, saveBook.name, UserLoanStatus.LOANED))
         val request = BookLoanRequest("김진욱", "이상한 나라의 앨리스")
 
         // when & then
@@ -90,7 +91,7 @@ class BookServiceTest @Autowired constructor(
         // given
         val saveBook = bookRepository.save(Book.fixture("이상한 나라의 앨리스"))
         val savedUser = userRepository.save(User("김진욱", null))
-        userLoanHistoryRepository.save(UserLoanHistory(savedUser, saveBook.name, false))
+        userLoanHistoryRepository.save(UserLoanHistory.fixture(savedUser, saveBook.name))
         val request = BookReturnRequest("김진욱", "이상한 나라의 앨리스")
 
         // when
@@ -99,6 +100,6 @@ class BookServiceTest @Autowired constructor(
         // then
         val results = userLoanHistoryRepository.findAll()
         assertThat(results).hasSize(1)
-        assertThat(results[0].isReturn).isTrue
+        assertThat(results[0].status).isEqualTo(UserLoanStatus.RETURNED)
     }
 }
