3155b86b2649214cd6c7021d165b159d85058c15
kimjinwook1 <rlawlsdnr628@naver.com>
2022-10-15 14:53:42 +0900

    유저 대출 현황 보여주기 개발

--- src/main/kotlin/com/group/libraryapp/controller/user/UserController.kt
+++ src/main/kotlin/com/group/libraryapp/controller/user/UserController.kt
@@ -2,6 +2,7 @@ package com.group.libraryapp.controller.user
 
 import com.group.libraryapp.dto.user.request.UserCreateRequest
 import com.group.libraryapp.dto.user.request.UserUpdateRequest
+import com.group.libraryapp.dto.user.response.UserLoanHistoryResponse
 import com.group.libraryapp.dto.user.response.UserResponse
 import com.group.libraryapp.service.user.UserService
 import org.springframework.web.bind.annotation.DeleteMapping
@@ -36,5 +37,10 @@ class UserController(
     fun deleteUser(@RequestParam name: String) {
         // @RequestParam을 사용할 때 타입을 nullable 하게 사용하면 required = false 로 변경한다.
         userService.deleteUser(name)
+    }
+
+    @GetMapping("/user/loan")
+    fun getUserLoanHistories(): List<UserLoanHistoryResponse> {
+        return userService.getUserLoanHistories()
     }
 }
